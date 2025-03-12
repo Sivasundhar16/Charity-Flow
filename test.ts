e697b3ab3c7b6d5ec0449b8925c19764ac520490
Sivasundhar16 <vssiva18@gmail.com>
2025-03-12 11:00:21 +0530

    fixing laptop

---
+++ test.ts
@@ -0,0 +1,50 @@
+Mar 12 10:51:42 Siva snapd[11487]: overlord.go:279: Acquired state lock file
+Mar 12 10:51:42 Siva snapd[11487]: daemon.go:250: started snapd/2.67.1+24.10 (series 16; classic) ubuntu/24.10 (amd64) linux/6.11.0-19-generic.
+Mar 12 10:51:42 Siva snapd[11487]: daemon.go:353: adjusting startup timeout by 30s (pessimistic estimate of 30s plus 5s per snap)
+Mar 12 10:51:42 Siva snapd[11487]: backends.go:58: AppArmor status: apparmor is enabled and all features are available
+Mar 12 10:51:42 Siva systemd[1]: Started snapd.service - Snap Daemon.
+Mar 12 10:51:47 Siva snapd[11487]: daemon.go:548: gracefully waiting for running hooks
+Mar 12 10:51:47 Siva snapd[11487]: daemon.go:550: done waiting for running hooks
+Mar 12 10:51:50 Siva snapd[11487]: overlord.go:518: Released state lock file
+Mar 12 10:51:50 Siva snapd[11487]: daemon stop requested to wait for socket activation
+Mar 12 10:51:50 Siva systemd[1]: snapd.service: Deactivated successfully.
+Mar 12 10:55:08 Siva systemd[1]: Starting snapd.service - Snap Daemon...
+Mar 12 10:55:08 Siva snapd[12000]: overlord.go:274: Acquiring state lock file
+Mar 12 10:55:08 Siva snapd[12000]: overlord.go:279: Acquired state lock file
+Mar 12 10:55:08 Siva snapd[12000]: daemon.go:250: started snapd/2.67.1+24.10 (series 16; classic) ubuntu/24.10 (amd64) linux/6.11.0-19-generic.
+Mar 12 10:55:08 Siva snapd[12000]: daemon.go:353: adjusting startup timeout by 30s (pessimistic estimate of 30s plus 5s per snap)
+Mar 12 10:55:08 Siva snapd[12000]: backends.go:58: AppArmor status: apparmor is enabled and all features are available
+Mar 12 10:55:08 Siva systemd[1]: Started snapd.service - Snap Daemon.
+Mar 12 10:55:14 Siva snapd[12000]: daemon.go:548: gracefully waiting for running hooks
+Mar 12 10:55:14 Siva snapd[12000]: daemon.go:550: done waiting for running hooks
+Mar 12 10:55:17 Siva snapd[12000]: overlord.go:518: Released state lock file
+Mar 12 10:55:17 Siva snapd[12000]: daemon stop requested to wait for socket activation
+Mar 12 10:55:17 Siva systemd[1]: snapd.service: Deactivated successfully.
+Mar 12 10:57:31 Siva systemd[1]: Starting snapd.service - Snap Daemon...
+Mar 12 10:57:32 Siva snapd[14039]: overlord.go:274: Acquiring state lock file
+Mar 12 10:57:32 Siva snapd[14039]: overlord.go:279: Acquired state lock file
+Mar 12 10:57:32 Siva snapd[14039]: daemon.go:250: started snapd/2.67.1+24.10 (series 16; classic) ubuntu/24.10 (amd64) linux/6.11.0-19-generic.
+Mar 12 10:57:32 Siva snapd[14039]: daemon.go:353: adjusting startup timeout by 30s (pessimistic estimate of 30s plus 5s per snap)
+Mar 12 10:57:32 Siva snapd[14039]: backends.go:58: AppArmor status: apparmor is enabled and all features are available
+Mar 12 10:57:33 Siva snapd[14039]: helpers.go:159: error trying to compare the snap system key: system-key missing on disk
+Mar 12 10:57:33 Siva systemd[1]: Started snapd.service - Snap Daemon.
+Mar 12 10:57:38 Siva snapd[14039]: daemon.go:548: gracefully waiting for running hooks
+Mar 12 10:57:38 Siva snapd[14039]: daemon.go:550: done waiting for running hooks
+Mar 12 10:57:41 Siva snapd[14039]: overlord.go:518: Released state lock file
+Mar 12 10:57:41 Siva snapd[14039]: daemon stop requested to wait for socket activation
+Mar 12 10:57:41 Siva systemd[1]: snapd.service: Deactivated successfully.
+Mar 12 10:57:58 Siva systemd[1]: Starting snapd.service - Snap Daemon...
+Mar 12 10:57:58 Siva snapd[14488]: overlord.go:274: Acquiring state lock file
+Mar 12 10:57:58 Siva snapd[14488]: overlord.go:279: Acquired state lock file
+Mar 12 10:57:58 Siva snapd[14488]: patch.go:64: Patching system state level 6 to sublevel 1...
+Mar 12 10:57:58 Siva snapd[14488]: patch.go:64: Patching system state level 6 to sublevel 2...
+Mar 12 10:57:58 Siva snapd[14488]: patch.go:64: Patching system state level 6 to sublevel 3...
+Mar 12 10:57:58 Siva snapd[14488]: daemon.go:250: started snapd/2.67.1+24.10 (series 16; classic) ubuntu/24.10 (amd64) linux/6.11.0-19-generic.
+Mar 12 10:57:58 Siva snapd[14488]: daemon.go:353: adjusting startup timeout by 30s (pessimistic estimate of 30s plus 5s per snap)
+Mar 12 10:57:58 Siva snapd[14488]: backends.go:58: AppArmor status: apparmor is enabled and all features are available
+Mar 12 10:57:58 Siva systemd[1]: Started snapd.service - Snap Daemon.
+Mar 12 10:58:04 Siva snapd[14488]: daemon.go:548: gracefully waiting for running hooks
+Mar 12 10:58:04 Siva snapd[14488]: daemon.go:550: done waiting for running hooks
+Mar 12 10:58:07 Siva snapd[14488]: overlord.go:518: Released state lock file
+Mar 12 10:58:07 Siva snapd[14488]: daemon stop requested to wait for socket activation
+Mar 12 10:58:07 Siva systemd[1]: snapd.service: Deactivated successfully.
